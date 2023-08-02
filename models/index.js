acea7f98f8b503f40169dfbe095b26695ab563b5
Brendan Aper <brendanaper@gmail.com>
2023-08-01 18:56:29 -0700

    finished endpoints for 'category-routes.js'

--- models/index.js
+++ models/index.js
@@ -9,19 +9,21 @@ Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
 // Categories have many Products
-Category.hasMany(Product)
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
-    unique: false
+    foreignKey: false,
   }
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
-    unique: false
+    foreignKey: 'tag_id'
   }
 })
 module.exports = {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,10 +4,8 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   try {
-    const category = await Category.findAll({Where: {Product}});
+    const category = await Category.findAll({ include: [{model: Product}] });
     if(!category) {
       res.status(400),json({ message: "no categories found"})
     }
@@ -17,21 +15,52 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const category = await Category.findByPk(req.params.id, { include: [{model: Product}] });
+    if(!category) {
+      res.status(400),json({ message: "no categories found"})
+    }
+    res.status(200).json({ category });
+  } catch(e) {
+    res.status(500).json({ message: "server error"})
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    if(!category) {
+      res.status(400),json({ message: "Category cannot be created"})
+    }
+    res.status(200).json({ category });
+  } catch(e) {
+    res.status(500).json({ message: "server error"})
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {
+    const category = await Category.update(req.body, { where: { id: req.params.id } });
+    if(!category) {
+      res.status(400),json({ message: "Failed to update"})
+    }
+    res.status(200).json({ category });
+  } catch(e) {
+    res.status(500).json({ message: "server error"})
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const category = await Category.destroy({ where: { id: req.params.id} });
+    if(!category) {
+      res.status(400),json({ message: "Failed to Delte"})
+    }
+    res.status(200).json({ category });
+  } catch(e) {
+    res.status(500).json({ message: "server error"})
+  }
 });
 
 module.exports = router;
